20229b8fbf9008b5df2b6390eea8a3679cf899ee
nniabb <niabilanishvili17@gmail.com>
2024-03-04 17:49:38 +0400

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ExpenseModule } from './expense/expense.module';
+
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb+srv://niania:nia12345@cluster0.31mpgxd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'),
+    ExpenseModule
+  ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
---
+++ src/expense/expense.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { CreateExpenseDto } from './dto/create-expense.dto';
+import { UpdateExpenseDto } from './dto/update-expense.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Expense } from './entities/expense.entity';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class ExpenseService {
+  constructor(@InjectModel(Expense.name) expenseModel: Model<Expense>){}
+  create(createExpenseDto: CreateExpenseDto) {
+    return 'This action adds a new expense';
+  }
+
+  findAll() {
+    return `This action returns all expense`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} expense`;
+  }
+
+  update(id: number, updateExpenseDto: UpdateExpenseDto) {
+    return `This action updates a #${id} expense`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} expense`;
+  }
+}
